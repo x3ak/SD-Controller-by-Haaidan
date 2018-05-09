30e5b05145c62fb91a252ec26ff5e98c7fdff335
haaidan <38711036+haaidan@users.noreply.github.com>
2018-05-09 20:36:26 -0300

    added simple configuration file

---
+++ configuration.js
@@ -0,0 +1,8 @@
+var channel = 'archonthewizard';
+var nickname = 'your_twitch_username_here';
+var oauthkey = 'oauth:your_oauthkey_here';
+
+
+//twitch player dimensions
+var width = '100%';
+var height = '550';  //pixels
