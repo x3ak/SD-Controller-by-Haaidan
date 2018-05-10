6b600c96dbefe5bf542087c7936db21d55eb4d69
haaidan <38711036+haaidan@users.noreply.github.com>
2018-05-09 21:18:11 -0300

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,8 +1,8 @@
-var channel = 'archonthewizard';
-var nickname = 'your_twitch_username_here';
-var oauthkey = 'oauth:your_oauthkey_here';
+var channel = 'archonthewizard';                //channel name
+var nickname = 'your_twitch_username_here';     //insert your twitch username 
+var oauthkey = 'oauth:your_oauthkey_here';      //insert your own oauthkey.  Generate yours here: https://twitchapps.com/tmi/
 
 
 //twitch player dimensions
-var width = '100%';
-var height = '550';  //pixels 
+var width = '100%';                             //defines the width of the twitch player. Default value is 100%
+var height = '550';                             //defines the height of the twith player. Default value is 550 pixels 
